e506201e28949616e5bdf91a50d1e4ed4d7899c4
Tom Lambert <tom.lambert.13@live.com>
2016-02-09 02:56:42 +0100

    StringAnalyer: Assembly Information

--- StringAnalyzer/StringAnalyzer/Properties/AssemblyInfo.cs
+++ StringAnalyzer/StringAnalyzer/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StringAnalyzer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("StringAnalyzer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tom Lambert (Koopakiller)")]
 [assembly: AssemblyProduct("StringAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Tom Lambert (Koopakiller) 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("de-DE")]
+

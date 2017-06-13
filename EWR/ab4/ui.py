c5f00d289caccf1eb4796d81e0dbcda031759b78
Koopakiller <koopakiller@live.de>
2017-06-13 16:55:29 +0200

    read_yesno default_input param

--- EWR/ab4/ui.py
+++ EWR/ab4/ui.py
@@ -1,5 +1,5 @@
 # Author: Tom Lambert
-# Content: Functions to interact with the user
+# Content: Functions to interact with the user (ui = User Interface)
 
 
 def read_integer(msg=None, error_msg=None):
@@ -20,11 +20,12 @@ def read_integer(msg=None, error_msg=None):
     return read_integer(msg)
 
 
-def read_yesno(msg="[Y/n]", error_msg="Unrecognized input!"):
+def read_yesno(msg="[Y/n]", error_msg="Unrecognized input!", default_input=None):
     """
     Asks the user to input yes or no.
     :param msg: The message, displayed to the user.
     :param error_msg: The message, displayed to the user, in case he did not entered "y", "yes", "n" or "no".
+    :param default_input: The value to return, in case the user entered "". Pass None to ask the user again.
     :return: True if he entered "y" or "yes"; otherwise False.
     """
     res = input(msg)
@@ -32,6 +33,8 @@ def read_yesno(msg="[Y/n]", error_msg="Unrecognized input!"):
         return True
     if res.lower() in ["n", "no"]:
         return False
+    if default_input is not None and res == "":
+        return default_input
 
     if error_msg is not None:
         print(error_msg)

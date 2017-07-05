9acccce8aea727ff63f681caf2380ca626c234cc
Koopakiller <koopakiller@live.de>
2017-07-05 14:07:16 +0200

    Simplified SortAlgorithm

--- EWR/ab6_v2/SortAlgorithms.py
+++ EWR/ab6_v2/SortAlgorithms.py
@@ -34,81 +34,6 @@ class SortAlgorithm(object):
         else:
             self.calls.update({ name: count})
 
-    def chunk(self, lst, n):
-        """
-        Splits a list in n chunks.
-        :param lst: The list to split.
-        :param n: The number of entry in 1 chunk.
-        :return: A list of chunks with items from lst.
-        """
-        self.increase_call_counter("chunk(n={0})".format(n))
-        k, m = divmod(len(lst), n)
-        return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
-
-    def swap(self, lst, i, j):
-        """
-        Swaps two items in the list.
-        :param lst: The list.
-        :param i: The index of the first item.
-        :param j: The index of the second item.
-        :return: Nothing.
-        """
-        # a swap equals 2 list assignments when there are no tuples and we need a help-variable to swap
-        self.increase_call_counter("swap (~2 List item assignments)")
-        lst[i], lst[j] = lst[j], lst[i]
-
-
-class MergeSort(SortAlgorithm):
-    """Provides an implementation for the Mergesort algorithm"""
-
-    def __init__(self):
-        super(MergeSort, self).__init__()
-        self.name = "Mergesort"
-
-    def merge(self, l_lst, r_lst):
-        """
-        Merges wo lists together into a sorted result list.
-        :param l_lst: List 1 (left) to merge.
-        :param r_lst: List 2 (right) to merge.
-        :return: A sorted merged list from l_lst and r_lst.
-        """
-        result = []
-        while len(l_lst) > 0 and len(r_lst) > 0:
-            self.increase_call_counter("Element Compare")
-            self.increase_call_counter("Get item from list", 2)
-            if l_lst[0] <= r_lst[0]:
-                self.increase_call_counter("Get item from list")
-                item = l_lst.pop(0)
-                self.increase_call_counter("Add item to result-list")
-                result.append(item)
-            else:
-                self.increase_call_counter("Get item from list")
-                item = r_lst.pop(0)
-                self.increase_call_counter("Add item to result-list")
-                result.append(item)
-        self.increase_call_counter("Split list into list1 < PivotItem(s) < list2")
-        self.increase_call_counter("Add multiple items to result-list")
-        result.extend(l_lst)
-        result.extend(r_lst)
-        return result
-
-    def sort(self, lst):
-        """
-        Sorts the list using the Mergesort algorithm.
-        :param lst: The list to sort.
-        :return: A sorted list with the items from lst.
-        """
-        if len(lst) <= 1:
-            return lst
-        chunks = list(self.chunk(lst, 2))
-
-        self.increase_call_counter("Recursive Sort-Call")
-        l_lst = self.sort(chunks[0])
-        self.increase_call_counter("Recursive Sort-Call")
-        r_lst = self.sort(chunks[1])
-
-        return self.merge(l_lst, r_lst)
-
 
 class QuickSort(SortAlgorithm):
     """Provides an implementation for the Quicksort algorithm"""
@@ -181,7 +106,8 @@ class GnomeSort(SortAlgorithm):
             if pos == 0 or lst[pos] >= lst[pos - 1]:
                 pos += 1
             else:
-                self.swap(lst, pos, pos - 1)
+                self.increase_call_counter("swap (~2 List item assignments)")
+                lst[pos], lst[pos - 1] = lst[pos - 1], lst[pos]
                 pos -= 1
         return lst
 
